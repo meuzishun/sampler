a03ea52a214fbc781708d549c458f2bb215a53cc
Andrew Smith <meuzishun@gmail.com>
2021-08-24 15:26:19 -0400

    Add sample.js file

--- sampler.js
+++ sampler.js
@@ -2,20 +2,30 @@ class Sampler {
     constructor() {
         this.audioCtx = new AudioContext();
         this.samplerGain = this.audioCtx.createGain();
+        this.samplerGain.connect(this.audioCtx.destination);
         this.samples = {};
     }
 
-    async addSample(filepath) {
+    async addSample(filepath, audioContext) {
         const buffer = this.audioCtx.createBufferSource();
         const response = await fetch(filepath);
         const arrayBuffer = await response.arrayBuffer();
-        buffer.buffer = await audioContext.decodeAudioData(arrayBuffer);
-        this.samples.filepath = buffer;
+        buffer.buffer = await this.audioCtx.decodeAudioData(arrayBuffer);
+        this.samples[filepath] = buffer;
     }
 
     removeSample() {}
 
-    playSample() {}
+    playSample(sample) {
+        const now = this.audioCtx.currentTime;
+
+        const volume = this.audioCtx.createGain();
+        sample.connect(volume).connect(this.samplerGain);
+        volume.gain.value = 0;
+
+        sample.start();
+        volume.gain.setTargetAtTime(1, now, 0.01);
+    }
 
     stopSample() {} 
 }
