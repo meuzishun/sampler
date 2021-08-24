39b304f5562e538316527654fc5535731fc47793
Andrew Smith <meuzishun@gmail.com>
2021-08-24 12:40:22 -0400

    Add mp3s

--- sampler.js
+++ sampler.js
@@ -2,5 +2,24 @@ class Sampler {
     constructor() {
         this.audioCtx = new AudioContext();
         this.samplerGain = this.audioCtx.createGain();
+        this.samples = {};
     }
+
+    async addSample(filepath) {
+        const buffer = this.audioCtx.createBufferSource();
+        const response = await fetch(filepath);
+        const arrayBuffer = await response.arrayBuffer();
+        buffer.buffer = await audioContext.decodeAudioData(arrayBuffer);
+        this.samples.filepath = buffer;
+    }
+
+    removeSample() {}
+
+    playSample() {}
+
+    stopSample() {} 
 }
+
+const mySampler = new Sampler();
+console.log(mySampler);
+
